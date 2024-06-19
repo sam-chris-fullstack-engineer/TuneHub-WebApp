ea097f3fd75b36d8d668b0f24b75f11d4ceedda8
sam-christopher07 <samchristophergit07@gmail.com>
2024-06-19 11:57:32 +0530

    init push

---
+++ src/test/java/com/kodnest/tunehubproject/TuneHub1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodnest.tunehubproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuneHub1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
